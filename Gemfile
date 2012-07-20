2eb3430bea5fdfb836cc815ad45ea59f04685e54
Tres Spicher <tres@tspike.com>
2012-07-20 14:53:07 -0600

    fixing the tests

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem 'capybara'
   gem 'capybara-webkit'
   gem 'launchy'
+  gem 'database_cleaner'
 end
 
 
--- spec/requests/site_spec.rb
+++ spec/requests/site_spec.rb
@@ -6,22 +6,34 @@ describe "Site" do
   end
 
   it "should have a field for your initials" do
-    page.should have_field 'Initials'
+    page.should have_field 'initial_initials'
   end
 
-  it "should return the right names" do
-    fill_in 'Initials', :with => 'TAS'
-    click_button 'Create Initial'
+  it "gives the correct skier name", :js => true do
+    fill_in 'initial_initials', :with => 'TAS'
+    wait_until { page.find('#result').text.split(/ /).length == 3 }
     page.should have_content 'Iffy Stash McFaceplant'
 
     visit root_path
-    fill_in 'Initials', :with => 'EWW'
-    click_button 'Create Initial'
+    fill_in 'initial_initials', :with => 'EWW'
+    wait_until { page.find('#result').text.split(/ /).length == 3 }
     page.should have_content 'Craggy Poser Camberson'
+  end
 
-    visit root_path
-    fill_in 'Initials', :with => 'sfx'
-    click_button 'Create Initial'
+  it "handles bad input", :js => true do
+    fill_in 'initial_initials', :with => '@*#**('
+    page.find('#result').text.should be_empty
+  end
+
+  it "handles lowercase", :js => true do
+    fill_in 'initial_initials', :with => 'sfx'
+    wait_until { binding.pry; page.find('#result').text.split(/ /).length == 3 }
+    page.should have_content 'Dank Glade Rockerson'
+  end
+
+  it "handles mixed case", :js => true do
+    fill_in 'initial_initials', :with => 'sFX'
+    wait_until { page.find('#result').text.split(/ /).length == 3 }
     page.should have_content 'Dank Glade Rockerson'
   end
 end
